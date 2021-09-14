8595138fed190f499467b98c1f1871f79af23d98
Koji Inoue <koji.gabriel218@gmail.com>
2021-09-15 01:06:24 +0300

    "advanced filtering on groupings of data, 1d and 2d tables"

--- external_functions.py
+++ external_functions.py
@@ -2,6 +2,32 @@ import pandas as pd
 import datetime
 import numpy as np
 
+def translate_data(data,column_name,agg_target,agg_type,visual,date_string=False):
+	#date requests always have index and column, but no dates have no column
+	#level adding for OrderID with subsequent grouping makes sure the values are correct 
+	#since the order has OrderDetailID which is a child of OrderID
+	if date_string:
+		data.index = data.index.strftime(date_string)
+		data = pd.pivot_table(data,index=data.index,columns=[column_name,'OrderID'],values=agg_target,aggfunc='sum').groupby(level=0,axis=1).agg(agg_type)
+	else:
+		data = pd.pivot_table(data,columns=[column_name,'OrderID'],values=agg_target,aggfunc='sum').groupby(level=0,axis=1).agg(agg_type)
+	data = data.fillna(0).round(2).sort_index()
+
+	#create a json serializable array which can be read on the highcharts app, with both axis
+	series = []
+	data = data[data.sum().sort_values(ascending=False).index]
+	for i in np.arange(0,len(data.columns)):
+		stuff = {'name':data.columns[i],'data':[ round(i,2)  for i in data[data.columns[i]]],'type':visual}
+		series.append(stuff)
+	
+	xAxis = {'categories':[i for i in data.index],'title':data.index.name}
+	json_data = {'series':series,'xAxis':xAxis}
+	
+	return json_data
+    
+
+'''
+
 def translate_data(data,date_string,column_name,agg_target,agg_type,visual,accum=False):
 	data.index = data.index.strftime(date_string)
 	data = pd.pivot_table(data,index=data.index,columns=[column_name],values=agg_target,aggfunc=agg_type).fillna(0)
@@ -17,10 +43,4 @@ def translate_data(data,date_string,column_name,agg_target,agg_type,visual,accum
 	
 	return json_data
     
-
-
-
-
-'''
-#stuff = {'name':data.columns[i],'data':[ round(i,2) if i > 0 else 'null' for i in data[data.columns[i]]],'type':'line'}
 '''
--- main.py
+++ main.py
@@ -23,21 +23,9 @@ def profile_validation():
 	meta_data = [{'name':i[0],'count':int(i[1]),'dtype':i[2].name}   for i in zip(data.nunique().index,data.nunique().values,data.dtypes)]
 	
 	#plug in the variables
-	json_data = external_functions.translate_data(data,'%B','SupplierName','Total','sum','scatter')
-	json_data['meta_data'] = meta_data
-	
-	#make a json array for charts, Y axis
-	'''
-	series = []
-	data = data[data.sum().sort_values(ascending=False).index]
-	for i in np.arange(0,len(data.columns)):
-		stuff = {'name':data.columns[i],'data':[ round(i,2)  for i in data[data.columns[i]]],'type':'scatter'}
-		#stuff = {'name':data.columns[i],'data':[ round(i,2) if i > 0 else 'null' for i in data[data.columns[i]]],'type':'line'}
-		series.append(stuff)
-	
-	#the X axis is the index and what it is formatted to
-	xAxis = {'categories':[i for i in data.index],'title':data.index.name}
-	'''
+	json_data = external_functions.translate_data(data,'EmployeeName','Total','sum','line','%m-%Y')
+	json_data['meta_data'] = meta_data	
+
 	return jsonify(json_data)
 
 @app.route("/")
