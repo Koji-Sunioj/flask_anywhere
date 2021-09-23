7a9b230153c25b10a86f0531e945b61bb3ef3636
Koji Inoue <koji.gabriel218@gmail.com>
2021-09-24 01:03:20 +0300

    working on the javascript part of the interface

--- main.py
+++ main.py
@@ -17,25 +17,34 @@ app.secret_key = 'ironpond_2'
 
 @app.route('/bi_data', methods=['GET'])
 def bi_data():
-	data = db_functions.sales()
-	
-	#get the name, count and data type for the select inputs on the page
-	meta_data = [{'name':i[0],'count':int(i[1]),'dtype':i[2].name}   for i in zip(data.nunique().index,data.nunique().values,data.dtypes)]
-	
-	#plug in the variables
-	highchart = external_functions.Highcharts('EmployeeName','Total','column','timeseries',agg_type='mean')
-	
-	if highchart.chart_type == 'correlation':
-		new_data = highchart.corr_frame(data)
-		new_json = highchart.corr_to_json(new_data)
-	else:
-		new_data = highchart.agg_frame(data)
-		new_json = highchart.agg_to_json(new_data)
-	new_json['meta_data'] = meta_data
+	if 'state' not in session:
+		#grab db data
+		data = db_functions.sales()
+		
+		#get the name, count and data type for the select inputs on the page
+		meta_data = [{'name':i[0],'count':int(i[1]),'dtype':i[2].name}   for i in zip(data.nunique().index,data.nunique().values,data.dtypes)]
+		meta_data.reverse()
+		
+		#plug in the variables
+		highchart = external_functions.Highcharts('ShipperName','CategoryName','scatter','correlation',corr_cat='Total')
+		
+		if highchart.chart_type == 'correlation':
+			new_data = highchart.corr_frame(data)
+			new_json = highchart.corr_to_json(new_data)
+		else:
+			new_data = highchart.agg_frame(data)
+			new_json = highchart.agg_to_json(new_data)
+		new_json['meta_data'] = meta_data
+		session['state'] = new_json
+		print('fresh load')
+	elif session['state']:
+		new_json = session['state']
+		print('already loaded')
 	return jsonify(new_json)
 
 @app.route("/")
 def hello_world():
+	#session.pop('state',None)
 	return render_template('index.html')
 
 
--- static/main.js
+++ static/main.js
@@ -1,63 +1,35 @@
 function bi_dashboard()
-{
+{   
+    //initial load based on on the data loaded either from the flask session or the fresh load without cookies
     $.get( "bi_data", function(data) {
-        
-        //send the columns to select input
-        //$(data.meta_data).each(function(index,value)
-       // {   
-       //     $('#col_count').append(`<option value=${value.name}>${value.name} (${value.count})</option>`)
-       // });
-       console.log(data.legend)
-       if (data.type == 'scatter')
-       {
+        $(data.meta_data).each(function(index,value)
+        {   var option = `<option value=${value.name} dtype=${value.dtype}>${value.name} (${value.count})</option>`
+            $('#x_select').append(option)
+            $('#y_select').append(option)
+
+        });
+        if (data.type == 'scatter')
+        {
            var symbol = 'circle'
-       }
-       else 
-       {
-        var symbol = symbol
-       }
+        }
+        else 
+        {
+            var symbol = symbol
+        }
     
        //render the chart
-      Highcharts.chart('sales', {
-        title: {
-            text: data.title
-        },
-        chart: {
-            type: data.type
-        },
-    
-        yAxis: data.yAxis,
-       
-    
-        xAxis: data.xAxis,
-    
-        
-       /* tooltip: {
-            formatter: function(){
-            if (data.type == 'scatter') 
-            {
-                {
-                    var string='<strong>'+this.series.name + '</strong><br>'+data.xAxis.title.text+': '+ this.x+'<br>'+this.y;
-                   
-                }
-            }
-            else
-            {
-                var string='x: '+this.x+', <br>: '+this.y+', date: '+this.series.name;
-            }
-                return string;
-            }
-            
-        },*/
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            title: {text: data.legend}
-        },
-    
-        plotOptions: { 
-
+        Highcharts.chart('sales', {
+            title: {text: data.title},
+            chart: {type: data.type},
+            yAxis: data.yAxis,
+            xAxis: data.xAxis,
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                title: {text: data.legend}
+            },
+            plotOptions: { 
                 series: {
                     marker: {
                        symbol: symbol,
@@ -71,45 +43,78 @@ function bi_dashboard()
                         }
                     },
                 }
-            
-            /*
-            series: { marker: {
-                radius: 1
             },
-                point: {
-                    events: {
-                        click: function () {
-                            console.log('Category: ' + this.category + ', value: ' + this.y);
+            series: data.series,
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 500
+                    },
+                    chartOptions: {
+                        legend: {
+                            layout: 'horizontal',
+                            align: 'center',
+                            verticalAlign: 'bottom'
                         }
                     }
-                },
-               
-                enableMouseTracking: true,
-                marker: {
-                    enabled: true
-                },dataGrouping: {
-                    enabled: false
-                 }
-            }*/
-        },
-    
-        series: data.series,
-    
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-    
+                }]
+            }
+        //highchart ends here 
+        }); 
+         
+    //get request ends here  
+    })
+
+    //undisable the select once clicked on
+    $(document).on('click','#correlation',function()
+    {
+        $('#x_select').prop('disabled',false);
+        $('#y_select').prop('disabled',false);
     });
-      
-}) };
+   
+
+    //event listener for handling disabling select of same column in other axis. 
+    $(document).on('change',['#x_select','#y_select'],function()
+    {
+
+        var x_selected = $('#x_select').val();
+        $('#y_select').find('option').each(function(index,value)
+        {  
+            if (x_selected == $(value).val() || $(value).val() == 'Y-Axis')
+            {
+                $(value).prop('disabled',true);
+            }        
+            else 
+            {
+                $(value).prop('disabled',false);
+            }
+        })
+        var y_selected = $('#y_select').val();
+        $('#x_select').find('option').each(function(index,value)
+        {   
+            if (y_selected == $(value).val() || $(value).val() == 'X-Axis')
+            {
+                $(value).prop('disabled',true);
+            }             
+            else 
+            {
+                $(value).prop('disabled',false);
+            }
+        })
+
+        //we show another column based on the data type of that selected option
+        var x_axis =  $('#x_select').find(":selected").attr('dtype');
+        var y_axis =  $('#y_select').find(":selected").attr('dtype');
+       
+        if (x_axis == 'object' && y_axis == 'object')
+        {
+           $('#variable_column').css('visibility','')
+        }
+        else 
+        {
+            $('#variable_column').css('visibility','hidden')
+        }
+     
+    });
+
+};
