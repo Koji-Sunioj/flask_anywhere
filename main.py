4fe6ce13ad33bc493096ac367182cd9f9d2a05ca
Koji Inoue <koji.gabriel218@gmail.com>
2021-09-27 22:28:09 +0300

    added ajax functionality with post method on data endpoint

--- main.py
+++ main.py
@@ -15,9 +15,31 @@ import external_functions
 app = Flask(__name__)
 app.secret_key = 'ironpond_2'
 
-@app.route('/bi_data', methods=['GET'])
+@app.route('/bi_data', methods=['GET','POST'])
 def bi_data():
-	if 'state' not in session:
+	if request.method == 'POST':
+		#send the form to a dictionary
+		send_values = {key:val for key,val in request.form.items()}
+		
+		#grab the data as usual
+		data = db_functions.sales()
+		
+		#we need metadata for the html elements and save in cookies
+		meta_data = [{'name':i[0],'count':int(i[1]),'dtype':i[2].name}   for i in zip(data.nunique().index,data.nunique().values,data.dtypes)]
+		meta_data.reverse()
+		
+		#handle the chart types
+		if send_values['type'] == 'correlation':
+			highchart = external_functions.Highcharts(send_values['x_axis'],send_values['y_axis'],send_values['visual'],send_values['type'])
+			if 'category' in send_values:
+				highchart.corr_cat = send_values['category']
+			new_data = highchart.corr_frame(data)
+			new_json = highchart.corr_to_json(new_data)
+		new_json['meta_data'] = meta_data
+		session['state'] = new_json
+		return jsonify(new_json)
+		
+	if request.method == 'GET' and 'state' not in session:
 		#grab db data
 		data = db_functions.sales()
 		#get the name, count and data type for the select inputs on the page
@@ -25,7 +47,7 @@ def bi_data():
 		meta_data.reverse()
 		
 		#plug in the variables
-		highchart = external_functions.Highcharts('ShipperName','Total','scatter','correlation')
+		highchart = external_functions.Highcharts('Price','Quantity','scatter','correlation',corr_cat='CategoryName')
 		
 		if highchart.chart_type == 'correlation':
 			new_data = highchart.corr_frame(data)
@@ -36,10 +58,11 @@ def bi_data():
 		new_json['meta_data'] = meta_data
 		session['state'] = new_json
 		print('fresh load')
-	elif session['state']:
+		return jsonify(new_json)
+	elif request.method == 'GET' and session['state']:
 		new_json = session['state']
 		print('already loaded')
-	return jsonify(new_json)
+		return jsonify(new_json)
 
 @app.route("/")
 def hello_world():
--- static/main.js
+++ static/main.js
@@ -1,13 +1,6 @@
 function bi_dashboard()
-{   
-    //initial load based on on the data loaded either from the flask session or the fresh load without cookies
-    $.get( "bi_data", function(data) {
-        $(data.meta_data).each(function(index,value)
-        {   var option = `<option value=${value.name} dtype=${value.dtype}>${value.name} (${value.count})</option>`
-            $('#x_select').append(option);
-            $('#y_select').append(option);
-
-        });
+{   function update_highchart(data)
+    {
         if (data.type == 'scatter')
         {
            var symbol = 'circle'
@@ -33,7 +26,7 @@ function bi_dashboard()
                 series: {
                     marker: {
                        symbol: symbol,
-                       radius: 5
+                       radius: 3
                     },
                     point: {
                         events: {
@@ -60,10 +53,23 @@ function bi_dashboard()
                 }]
             }
         //highchart ends here 
-        }); 
+        });   
+    }
+    //initial load based on on the data loaded either from the flask session or the fresh load without cookies
+    $.get( "bi_data", function(data) {
+        $(data.meta_data).each(function(index,value)
+        {   var option = `<option value=${value.name} dtype=${value.dtype}>${value.name} (${value.count})</option>`
+            $('#x_select').append(option);
+            $('#y_select').append(option);
+
+        });
+        update_highchart(data)
          
     //get request ends here  
     })
+
+    
+
 
     //undisable the select once clicked on
     $(document).on('click','#correlation',function()
@@ -128,19 +134,60 @@ function bi_dashboard()
             normalize_variable_column();
             return true
         }
-
-        
-
     }
-    /*$(document).on('change','#variable_column[name=value]',function()
-    { 
-        
-        if ($('#variable_column[name=value]').val())
+
+    function manage_val_column(x_selected,y_selected)
+    {
+        if ($('#variable_column[name=value]').val() != null && x_selected != null  &&  y_selected != null && x_selected != y_selected )
         {
             $('#send_values').prop('disabled',false)
         }
+    
+        else if ($('#variable_column[name=value]').length == 0 && x_selected != null  &&  y_selected != null && x_selected != y_selected )
+        {  
+            $('#send_values').prop('disabled',false) 
+        }
+        else 
+        {
+            $('#send_values').prop('disabled',true)
+        } 
+    }
+    $(document).on('click','#send_values', function() {
 
-    });*/
+        if ($('#correlation').is(':checked'))
+        {
+            data = {
+                x_axis : $('#x_select').val(),
+                y_axis :$('#y_select').val(),
+                visual : 'scatter',
+                type : 'correlation'
+            }
+            if ($('#variable_column').val())
+            {
+                data.category = $('#variable_column').val()
+            }
+        }
+		
+        $.ajax({
+			data :data,
+			type : 'POST',
+			url : '/bi_data'
+		})
+        
+        .done(function(data){ 
+            {
+             update_highchart(data)
+            }  
+        });
+    })
+
+    $(document).on('change','#variable_column[name=value]',function()
+    { 
+        var x_selected = $('#x_select').val();
+        var y_selected = $('#y_select').val();
+        //handle visibility of submit button
+        manage_val_column(x_selected,y_selected);
+    });
     
     $(document).on('change','#x_select',function()
     {   
@@ -162,8 +209,8 @@ function bi_dashboard()
         else
         {
             $('#x_select').css('color','').attr('title','')
-            $('#y_select').css('color','').attr('title','')
             $(`#x_select option:contains('${x_selected}')`).css('color','green').attr('title','selected in X Axis');
+            $('#y_select').css('color','').attr('title','')
             $(`#y_select option:contains('${y_selected}')`).css('color','green').attr('title','selected in Y Axis');
         }
     
@@ -178,29 +225,19 @@ function bi_dashboard()
                 manage_corr_cat('64','value','category','object');
             }
         }
-        
-         /*
-        if ($('#variable_column[name=value]').val() === null) //| $('#variable_column[name=value]').val() === undefined
-        {
-                $('#send_values').prop('disabled',true)
-        }
-       
-        else if (!String(x_selected).includes('Axis') && !String(y_selected).includes('Axis'))
-        {
-            $('#send_values').prop('disabled',false)
-        }  */
+        //handle visibility of submit button
+        manage_val_column(x_selected,y_selected)
     });
 
     $(document).on('change','#y_select',function()
     {   
-        
-        
         var x_selected = $('#x_select').val();
         var y_selected = $('#y_select').val();
 
         //visuals from the column level, highlight error and disable button
         $('#x_select option').css('color','black')
         $('#y_select option').css('color','black')
+
         if (x_selected == y_selected)
         {
             $('#x_select').css('color','red').attr('title','both Axes are the same')
@@ -214,8 +251,8 @@ function bi_dashboard()
         else
         {
             $('#x_select').css('color','').attr('title','')
-            $('#y_select').css('color','').attr('title','')
             $(`#x_select option:contains('${x_selected}')`).css('color','green').attr('title','selected in X Axis');
+            $('#y_select').css('color','').attr('title','')
             $(`#y_select option:contains('${y_selected}')`).css('color','green').attr('title','selected in Y Axis');
         }
         //handling the variable column if both axes are the same data type
@@ -229,20 +266,8 @@ function bi_dashboard()
                 manage_corr_cat('64','value','category','object');
             }
         }
-    
-        /*
-        if ($('#variable_column[name=value]').val() === null) //| $('#variable_column[name=value]').val() === undefined
-            {
-                $('#send_values').prop('disabled',true)
-            }
-            
-        else if (!String(x_selected).includes('Axis') && !String(y_selected).includes('Axis'))
-        {
-            $('#send_values').prop('disabled',false)
-        }*/
-
-       
-        
+        //handle visibility of submit button
+        manage_val_column(x_selected,y_selected)
     });
 };
 
